86cd0863cea36ee400cb77dba326d0b1b241e261
Luciano Maciel <lucianomaciel53@gmail.com>
2026-04-02 12:27:49 -0400

    Create ex4.c

---
+++ Lista de Estrutura Sequencial/ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (){
+    setlocale(LC_ALL, "");
+
+    float nota1, nota2, nota3, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+    media = (nota1 + nota2 + nota3) / 3;
+    printf("A média das notas é: %.2f\n", media);
+    return 0;
+}
