02223cc99e0c2426b0865088495c4a65f597ff4c
Luciano Maciel <lucianomaciel53@gmail.com>
2026-04-02 12:28:46 -0400

    Update ex4.c

--- Lista de Estrutura Sequencial/ex4.c
+++ Lista de Estrutura Sequencial/ex4.c
@@ -7,11 +7,11 @@ int main (){
 
     float nota1, nota2, nota3, media;
 
-    printf("Digite a primeira nota: ");
+    printf("Digite a primeira nota: \n");
     scanf("%f", &nota1);
-    printf("Digite a segunda nota: ");
+    printf("Digite a segunda nota: \n");
     scanf("%f", &nota2);
-    printf("Digite a terceira nota: ");
+    printf("Digite a terceira nota: \n");
     scanf("%f", &nota3);
     media = (nota1 + nota2 + nota3) / 3;
     printf("A média das notas é: %.2f\n", media);
