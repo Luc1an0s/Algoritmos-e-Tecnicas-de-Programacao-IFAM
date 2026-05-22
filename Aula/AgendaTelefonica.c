032fa53686f26f7efd0f2dda3811ed9f632548f4
Luciano Maciel <lucianomaciel53@gmail.com>
2026-05-21 22:12:37 -0400

    Update AgendaTelefonica.c

--- Aula/AgendaTelefonica.c
+++ Aula/AgendaTelefonica.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+
+enum semana{Domingo = 10, Segunda, Terca, Quarta, Quinta, Sexta, Sabado};
+
+int main(){
+    enum semana s1, s2, s3;
+
+    s1=Segunda;
+    s2=Terca;
+
+    s3=s1+s2;
+
+    printf("Domingo = %d\n", Domingo);
+    printf("Segunda = %d\n", Segunda);
+    printf("Terca = %d\n", Terca);#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
@@ -65,6 +79,8 @@ void buscarContatoPorNome(Contato agenda[], int total);
 void atualizarContato(Contato agenda[], int total);
 void removerContato(Contato agenda[], int total);
 void alterarContato(Contato agenda[], int total);
+void listarFavoritos(Contato agenda[], int total);
+ 
 int encontrarIndicePorId(Contato agenda[], int total);
 const char* tipoParaTexto(TipoContato tipo);
 TipoContato escolherTipoContato();
@@ -158,7 +174,115 @@ TipoContato escolherTipoContato(){
     return (TipoContato)opcao;
 }
 
+void imprimirContato(Contato contato){
+    printf("\n%s------------------------------%s\n", AZUL, RESET);
+    printf("\n%sID: %s %d", NEGRITO, RESET, contato.id);
+    printf("\n%Nome: %s %s", NEGRITO, RESET, contato.nome);
+    printf("\n%sTelefone: %s %s", NEGRITO, RESET, contato.telefone);
+    printf("\n%E-mail: %s %s", NEGRITO, RESET, contato.email);
+    printf("%sFavorito:%s %s\n",
+           NEGRITO,
+           RESET,
+           contato.favorito ? "Sim" : "Não");
+    printf("\n%s------------------------------%s\n", AZUL, RESET);
+}
+
+void listarContatos(Contato agenda[], int total){
+    int encontrou = 0;
+    
+    printf("\n%s=== Lista de Contatos ====%s\n", NEGRITO, RESET);
+    
+    for(int = 0; i < total; i++){
+        if(agenda[i].ativo == 1){
+            imprimirContato(agenda[i]);
+            encontrou =1;
+        }
+    }
+    if(!encontrou){
+        printf("\n%sNenhum contato cadastrado.%s\n", VERMELHO, RESET);
+    }    
+}
+
+void buscarContatoPorNome(Contato agenda[], int total){
+    char busca[TAM_NOME];
+    int encontrou = 0;
+    
+    printf("\n%s==== Buscar Contato ====%s\n", NEGRITO, RESET);
+    lerTexto("Digite parte do nome: ", busca, TAM_NOME)
+    for(int = 0; i < total; i++){
+        if(agenda[i].ativo == 1 && strstr(agenda[i].nome, busca) != NULL){
+            imprimirContato(agenda[i]);
+            encontrou =1;
+        }
+    }
+    if(!encontrou){
+        printf("\n%sNenhum contato encontrado.%s\n", VERMELHO, RESET);
+    } 
+}
+
+int encontrarIndicePorId(Contato agenda[], int total){
+    int busca;
+    
+    printf("\n%s==== Buscar Contato ====%s\n", NEGRITO, RESET);
+    busca = lerInteiro("Digite o id");
+    
+    for(int = 0; i < total; i++){
+        if(agenda[i].ativo == 1 && agenda[i].id == busca) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+void atualizarContato(Contato agenda[], int total){
+    int id;
+    int indice;
+    
+    printf("\n%s===== Atualizar Contato =====%s\n", NEGRITO, RESET);
+    listarContatos(agenda, total);
+    
+    id = lerInteiro("\nDigite o id do contato que deseja atualizar ");
+    indice = encontrarIndicePorId(agenda, total, id);
+    
+    if(indice == -1){
+        printf("%sContato não encontrado%s\n", VERMELHO, RESET);
+        return;
+    } 
+    printf("\n%sDigite os novos dados do contato%s\n", AMARELO, RESET);
+    lerTexto(,TAM_NOME);
+    lerTexto(,TAM_TELEFONE);
+    lerTexto(,TAM_EMAIL);
+    lerTexto(,TAM_TEXTO);
+    lerTexto(,TAM_TEXTO);
+    lerTexto(,TAM_TEXTO);
+}
+
+void listarFavoritos(Contato agenda[], int total){
+    int encontrou = 0;
+    
+    printf("\n%s==== Contatos Fvoritos ====%s\n", NEGRITO, RESET)        ;
+    
+    for(int i =0; i< total; i++){
+        if(agenda[i].ativo == 1 && agenda[i].favorito = 1){
+            imprimirContato(agenda[i]);
+            encontrou = 1;
+        }            
+    }
+    if(!encontrou){
+        printf("\n%sNenhum contato favoritado.%s\n", VERMELHO, RESET);
+    } 
+}
 int main (){
+
+    return 0;
+}
+    printf("Quarta = %d\n", Quarta);
+    printf("Quinta = %d\n", Quinta);
+    printf("Sexta = %d\n", Sexta);
+    printf("Sabado = %d\n", Sabado);
+    printf("s1 = %d\n", s1);
+    printf("s2 = %d\n", s2);
+    printf("s3 = %d\n", s3);
 
     return 0;
 }
