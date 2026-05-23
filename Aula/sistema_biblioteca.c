156e549c23a78e33278678300b17afe6a58ce7f0
Luciano Maciel <lucianomaciel53@gmail.com>
2026-05-23 14:08:44 -0400

    definição de cores

--- Aula/sistema_biblioteca.c
+++ Aula/sistema_biblioteca.c
@@ -12,5 +12,11 @@
 #define TAM_NOME 80
 #define TAM_CPF 15
 
+//definição de cores
+#define VERDE "\x1b[32m"
+#define VERMELHO "\x1b[31m"
+#define AMARELO "\x1b[33m"
+#define CIANO "\x1b[35m"
+#define RESET "\x1b[0m"
 
 int main() {}
