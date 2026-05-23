1394c4649a013973025ee9109bcf26a5ffd15a1c
Luciano Maciel <lucianomaciel53@gmail.com>
2026-05-23 14:03:43 -0400

    definição de constantes

--- Aula/sistema_biblioteca.c
+++ Aula/sistema_biblioteca.c
@@ -3,4 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 
+//definição de constantes
+#define MAX_LIVROS 200
+#define MAX_USUARIOS 100
+#define TAM_TITULO 120
+#define TAM_AUTOR 80
+#define TAM_ISBN 20
+#define TAM_NOME 80
+#define TAM_CPF 15
+
+
 int main() {}
