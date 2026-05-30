91aaeac66dec25b4e336a7b209073522f4f9e9bf
Luciano Maciel <lucianomaciel53@gmail.com>
2026-05-29 20:46:50 -0400

    Update Desafio2.c
    Implement custom string length function and use it in main.

--- Desafios/Desafio2.c
+++ Desafios/Desafio2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#define TAM 50
+size_t meu_strlen(const char *s);
 
+size_t meu_strlen(const char *s) {
+    const char *cont = s;
+    while (*s != '\0') {
+        s++;
+    }
+    return s - cont;
+}
+
+int main(){
+    int i;
+    char palavra[TAM];
+
+    printf("Digite a palavra:\n");
+    fgets(palavra, TAM, stdin);
+    palavra[strcspn(palavra,"\n")]='\0';
+
+    i = meu_strlen(palavra);
+    printf("contador: %d", i);
+
+    return 0;
+}
