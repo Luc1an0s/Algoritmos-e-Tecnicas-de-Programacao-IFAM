25a7e0faeb4b9162bb5140eb16848fc695069c83
Luciano Maciel <lucianomaciel53@gmail.com>
2026-06-24 15:13:47 -0400

    Create ex1-alocmem.c

---
+++ Revisao-Prova/ex1-alocmem.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i;
+    int *vetor; 
+
+    printf("Quantos números você quer guardar? ");
+    scanf("%d", &n);
+
+    vetor = malloc(n * sizeof(int));
+
+    if (vetor == NULL) {
+        printf("Erro: Memória insuficiente!\n");
+        return 1; 
+
+    }
+    for(i = 0; i < n; i++) {
+        vetor[i] = (i + 1) * 10;
+        printf("vetor[%d] = %d\n", i, vetor[i]);
+    }
+
+    free(vetor);
+
+    return 0;
+}
